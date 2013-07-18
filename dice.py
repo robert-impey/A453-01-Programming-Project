78f8d2c524171c12c16dab9cc46871ece0157700
Robert Impey <robert@impey.info>
2013-07-18 13:55:55 +0100

    Used recursive functions to get the number of sides.

--- dice.py
+++ dice.py
@@ -1,32 +1,43 @@
-import re
 from random import randint
 
 def get_sides():
-    while True:
-        print """
+    print """
 You can have 4, 6 or 12 sides
 How many sides do you want?"""
-        
-        sides_raw = raw_input()
-        
-        sides = int(sides_raw)
     
-        if sides == 4 or sides == 6 or sides == 12:
-            return sides
+    sides_raw = raw_input()
+    
+    sides = int(sides_raw)
+
+    if sides == 4 or sides == 6 or sides == 12:
+        return sides
+    else:
+        return get_sides()
+
+def get_yes_no(question):
+    print question
+    
+    answer = raw_input()
+    
+    if answer.upper() == 'Y' or answer.upper() == "YES":
+        return True
+    elif answer.upper() == 'N' or answer.upper() == "NO":
+        return False
+    else:
+        print "Please enter either 'Y(es)' or 'N(o)'"
+        return get_yes_no(question)
+
+def play():
+    sides = get_sides()
+    
+    score = randint(1, sides)
+    print "%d sided dice thrown, score %d" % (sides, score)
+
+    keep_playing = get_yes_no("Do you want to keep playing?")
+    
+    if keep_playing:
+        play()
 
 print """Welcome to the game!
 You need to set the number of sides for the dice."""
-
-sides = get_sides()
-
-keep_playing = True
-keep_playing_regex = re.compile("^y(es)?$", re.IGNORECASE)
-
-while keep_playing:
-    score = randint(1, sides)
-    print "%d sided dice thrown, score %d" % (sides, score)
-    
-    print "Do you want to keep playing?"
-    keep_playing_raw = raw_input()
-    
-    keep_playing = keep_playing_regex.match(keep_playing_raw)
+play()
