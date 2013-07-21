50cd34e9efc060c78554fcd8f3e75de8bfe0d6cc
Robert Impey <robert.impey@gmail.com>
2013-07-21 18:04:50 +0100

    Made the dice rolling code more object oriented.

--- dice.py
+++ dice.py
@@ -1,3 +1,47 @@
-from rpg import play
+from random import randint
+from rpg import get_yes_no
 
-play()
+class SidesNotNumberError(ValueError):
+    pass
+
+class IllegalSidesError(ValueError):
+    pass
+
+class Die:
+    def __init__(self, sides):
+        if not sides.isdigit():
+            raise SidesNotNumberError
+        sides = int(sides)
+        if not (sides == 4 or sides == 6 or sides == 12):
+            raise IllegalSidesError
+        self.sides = sides
+    def roll(self):
+        return randint(1, self.sides)
+
+def create_die_for_user():
+    print """You need to set the number of sides for the dice.
+How many sides do you want?"""
+    
+    sides = raw_input()
+    
+    try:
+        return Die(sides)
+    except SidesNotNumberError:
+        print "Sides must be a positive, whole number!"
+        return create_die_for_user()
+    except IllegalSidesError:
+        print "Sides must be 4, 6 or 12"
+        return create_die_for_user()
+
+def play():
+    die = create_die_for_user()
+    score = die.roll()
+    print "%d sided dice thrown, score %d" % (die.sides, score)
+
+    keep_playing = get_yes_no("Do you want to keep playing?")
+    
+    if keep_playing:
+        play()
+
+if __name__ == "__main__":
+    play()
--- rpg.py
+++ rpg.py
@@ -1,23 +1,6 @@
 from random import randint
 import math
 
-def get_sides():
-    print """You need to set the number of sides for the dice.
-You can have 4, 6 or 12 sides
-How many sides do you want?"""
-    
-    sides_raw = raw_input()
-    
-    if not sides_raw.isdigit():
-        return get_sides()
-    
-    sides = int(sides_raw)
-    
-    if not (sides == 4 or sides == 6 or sides == 12):
-        return get_sides()
-    
-    return sides 
-
 def get_yes_no(question):
     print question
     
@@ -30,17 +13,6 @@ def get_yes_no(question):
     else:
         print "Please enter either 'Y(es)' or 'N(o)'"
         return get_yes_no(question)
-
-def play():
-    sides = get_sides()
-    
-    score = randint(1, sides)
-    print "%d sided dice thrown, score %d" % (sides, score)
-
-    keep_playing = get_yes_no("Do you want to keep playing?")
-    
-    if keep_playing:
-        play()
 
 def get_attribute_level():
     initial_value = 10
