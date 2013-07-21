5e3b498fa2a997db412fcf3bee517bd717453b41
Robert Impey <robert.impey@gmail.com>
2013-07-21 23:06:35 +0100

    Encounter code.

--- characters.py
+++ characters.py
@@ -1,4 +1,4 @@
-from random import randint
+from dice import Die
 from math import floor
 
 class Character:
@@ -6,18 +6,32 @@ class Character:
         self._name = name
         self._strength = get_attribute_level()
         self._skill = get_attribute_level()
+        self._alive = True
     def get_name(self):
         return self._name
     def get_strength(self):
         return self._strength
+    def set_strength(self, value):
+        if value > 0:
+            self._strength = value
+        else:
+            self._alive = True
     def get_skill(self):
         return self._skill
+    def set_skill(self, value):
+        if value < 0:
+            value = 0
+        self._skill = value
+    def is_alive(self):
+        return self._alive
 
 def get_attribute_level():
     initial_value = 10
     
-    score_of_12 = randint(1, 12)
-    score_of_4 = randint(1, 4)
+    die_12 = Die(12)
+    score_of_12 = die_12.roll()
+    die_4 = Die(4)
+    score_of_4 = die_4.roll()
     change = floor(score_of_12 / score_of_4)
     
     return initial_value + change
