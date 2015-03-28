3c9ea51bca502f140ad9aa0c572ee6f091506f7d
Robert Impey <robert@impey.info>
2015-03-28 16:04:00 +0000

    Moved dice rolling to function for use in REPL

--- dice.fsx
+++ dice.fsx
@@ -1,16 +1,16 @@
 open System
 
-if fsi.CommandLineArgs.Length = 2 then
+let roll sides = 
+    match sides with
+    | 4 | 6 | 12 -> 
+        let rng = Random()
+        let result = (rng.Next() % sides) + 1
+        printfn "You rolled a %d with a %d sided die." result sides
+    | _ -> printfn "The number of sides should be 4, 6 or 12!"
+
+if fsi.CommandLineArgs.Length = 2 then 
     let sidesRaw = fsi.CommandLineArgs.[1]
     let parsed, sides = Int32.TryParse sidesRaw
-    
-    if parsed then
-        match sides with
-        | 4 | 6 | 12 ->
-            let rng = Random()
-            let result = (rng.Next() % sides) + 1
-            
-            printfn "You rolled a %d with a %d sided die." result sides
-        | _ -> printfn "The number of sides should be 4, 6 or 12!"
+    if parsed then roll sides
     else printfn "You need to give me an integer!"
 else printfn "How many sides?"
